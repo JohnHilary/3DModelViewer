ace320ed49c180f037ff6b1e0085bc1ac33cecc7
John Hilary <john.h@codelynks.com>
2026-09-21 23:54:57 +0530

    feat:add theme for topbar

--- app/src/main/java/com/threed/modelviewer/MainActivity.kt
+++ app/src/main/java/com/threed/modelviewer/MainActivity.kt
@@ -5,19 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.threed.modelviewer.ui.theme._3DModelViewerTheme
 
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             _3DModelViewerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+                    CenterAlignedTopAppBar(title = {
+                        Text("3DModelViewer")
+                    }, colors =     TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.onBackground
+                    ))
+                }) { paddingValues ->
                     ModelViewerScreen(paddingValues)
                 }
             }
